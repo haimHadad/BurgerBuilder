ffb96162247dc54082eb88572b65193a91039bc9
HobbitWarrior <HobbitWarrior@users.noreply.github.com>
2021-01-02 13:31:55 +0200

    Buttons

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import Aux from '../../../hoc/Auxiliary';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) =>{
     const ingredientSummary = Object.keys(props.ingredients)
@@ -17,7 +19,11 @@ const orderSummary = (props) =>{
         <ul>
             {ingredientSummary}
         </ul>
+    <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
         <p>Continue To Checkout?</p>
+        <Button  btnType ="Danger" clicked= {props.purchaseCanceled}>CANCEL</Button>
+        <Button btnType ="Success" clicked= {props.purchaseContinued}>CONTINUE</Button>
+        
     </Aux>
     );
 };
