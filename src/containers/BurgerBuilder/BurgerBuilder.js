f85bb3dd58e5b59bef463051210c0cf7d2f6be8e
HobbitWarrior <HobbitWarrior@users.noreply.github.com>
2021-01-09 14:36:40 +0200

    handling error in request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,17 +18,23 @@ const INGREDIENT_PRICES = {
 
 class BurgerBuilder extends Component{
     state ={
-        ingredients:{
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        },
+        ingredients:null,
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
     };
+
+    componentDidMount(){
+        axios.get('https://react-my-burger-a69cd-default-rtdb.firebaseio.com/ingredient.json')
+        .then(response => {
+            this.setState({ingredients: response.data})
+        })
+        .catch(error => {
+            this.setState({error: true});
+        })
+    }
 
     updatePurchaseState(ingredients){ 
        /* const sum = Object.values(ingredients).reduce((a, b) => a + b, 0); */
@@ -112,27 +118,42 @@ class BurgerBuilder extends Component{
         for(let key in disabledInfo){
             disabledInfo[key] = disabledInfo[key] <= 0
         }
-        let orderSummary = <OrderSummary 
-                                ingredients={this.state.ingredients}
-                                price={this.state.totalPrice}
-                                purchaseCanceled={this.purchaseCancelHandler}
-                                purchaseContinued = {this.purchaseContinueHandler}/>
-        if(this.state.loading){
-            orderSummary = <Spinner/>
-        }
-        return (
-            <Aux>
-                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                   {orderSummary} 
-                </Modal>
-                <Burger ingredients={this.state.ingredients} />
-                <BuildControls 
+        
+        let orderSummary = null; 
+        let burger = this.state.error ? <p style={{textAlign:'center'}}>Ingredient can't be loaded!</p> : <Spinner/>;
+        if(this.state.ingredients){
+            burger = (
+                <Aux>
+                    <Burger ingredients={this.state.ingredients} />
+                    <BuildControls 
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
                     ordered={this.purchaseHandler}
                     price={this.state.totalPrice}/>
+                </Aux>
+            );
+            
+            orderSummary = (
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        price={this.state.totalPrice}
+                        purchaseCanceled={this.purchaseCancelHandler}
+                        purchaseContinued = {this.purchaseContinueHandler}/>
+            );
+   
+        if(this.state.loading){
+            orderSummary = <Spinner/>
+        }
+        }
+
+        return (
+            <Aux>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                   {orderSummary} 
+                </Modal>               
+                {burger}
             </Aux>
         );
     }
