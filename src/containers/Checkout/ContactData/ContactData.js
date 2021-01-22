22102acbfd0a0f91a2f5f7093bebd0d6e71b452f
HobbitWarrior <HobbitWarrior@users.noreply.github.com>
2021-01-22 23:59:45 +0200

    validtion

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    requierd: true
+                },
+                valid:false
             },
             street: {
                 elementType: 'input',
@@ -22,7 +26,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    requierd: true
+                },
+                valid:false
             },
             zipcode: {
                 elementType: 'input',
@@ -30,7 +38,13 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'ZIP Code'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    requierd: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid:false
             },
             country: {
                 elementType: 'input',
@@ -38,7 +52,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    requierd: true
+                },
+                valid:false
             },
             email: {
                 elementType: 'input',
@@ -46,7 +64,11 @@ class ContactData extends Component{
                     type: 'email',
                     placeholder: 'Your Email'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    requierd: true
+                },
+                valid:false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -90,11 +112,30 @@ class ContactData extends Component{
 
     }
 
+    checkValidity(value, rules){
+        let isValid = true;
+
+        if(rules.requierd){
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+        return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) =>{
         const updatedOrderForm = {...this.state.orderForm};
         const updatedFormElement = {...updatedOrderForm[inputIdentifier]}
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        
         this.setState({orderForm : updatedOrderForm});
     }
 
