573dd14180387b9ce488cc44ebad0f32df84acab
HobbitWarrior <haim.hddd@gmail.com>
2021-04-07 20:08:47 +0300

    hooks-modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 
 
-class Modal extends Component{
-
-    shouldComponentUpdate(nextProps, nextState){
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;   
-    }
-
-    componentWillUpdate(){
-    }
-
-    render(){
-        return (
-            <Aux>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-                <div 
-                    className={classes.Modal}
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show? '1' : '0'
-                    }}
-                    >
-                    {this.props.children}
-                </div>
-            </Aux>
-        );
-    }
+const modal = props =>{
+    return(
+        <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+        <div 
+            className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show? '1' : '0'
+            }}
+            >
+            {props.children}
+        </div>
+    </Aux>
+    );
+    
+    
 }
 
-export default Modal;
+export default React.memo(modal, 
+    (prevProps, nextProps) => 
+    nextProps.show === prevProps && 
+    nextProps.children === prevProps.children); 
+    
+    //At shouldComponentUpdate we checked whether we want to continue  
+    //with the update or not and we only wanted to continue if the props are not equal
+
+    //Now
